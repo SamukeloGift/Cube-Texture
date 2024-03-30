213de8eaeb088f63104d2f2bf5bbcae0b573034d
82Sam <53128739+Ivyson@users.noreply.github.com>
2024-03-30 23:40:57 +0200

    !

--- script.js
+++ script.js
@@ -29,10 +29,62 @@ let cubeVert = new Float32Array([
   //Buttom side
   0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -0.5,
 ]);
+let Texturecod = new Float32Array([
+    // Front face
+    0.0, 1.0,
+    1.0, 1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+
+    // Back face
+    1.0, 1.0,
+    0.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+
+    // Top face
+    0.0, 1.0,
+    1.0, 1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+
+    // // Bottom face
+    0.0, 1.0,
+    1.0, 1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+
+    // // Left face
+    0.0, 1.0,
+    1.0, 1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+
+    // // Right face
+    0.0, 1.0,
+    1.0, 1.0,
+    0.0, 0.0,
+    1.0, 0.0
+]);
 webgl.enable(webgl.DEPTH_TEST);
 let buffer = webgl.createBuffer();
 webgl.bindBuffer(webgl.ARRAY_BUFFER, buffer);
 webgl.bufferData(webgl.ARRAY_BUFFER, cubeVert, webgl.STATIC_DRAW);
+
+//Texture Buffer
+let texcoBuffer = webgl.createBuffer();
+webgl.bindBuffer(webgl.ARRAY_BUFFER, texcoBuffer);
+webgl.bufferData(webgl.ARRAY_BUFFER, Texturecod, webgl.STATIC_DRAW);
+
+//Creating Texture
+let textBufer = webgl.createTexture();
+webgl.bindTexture(webgl.TEXTURE_2D, textBufer);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MAG_FILTER, webgl.LINEAR);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MIN_FILTER, webgl.LINEAR);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_WRAP_S, webgl.CLAMP_TO_EDGE);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_WRAP_T, webgl.CLAMP_TO_EDGE);
+
+webgl.texImage2D(webgl.TEXTURE_2D, 0, webgl.RGBA, webgl.RGBA, webgl.UNSIGNED_BYTE, image);
 
 let vsShader = `
 precision mediump float;
@@ -40,8 +92,11 @@ attribute vec3 vecposition;
 uniform mat4 rotateY;
 uniform mat4 rotateX;
 uniform mat4 rotateZ;
+attribute vec2 vtexture;
+varying vec2 fragtexture;
 void main()
 {
+    fragtexture = vtexture;
     gl_Position = rotateX*rotateY*vec4(vecposition, 1.0);
     gl_PointSize = 5.0;
 }
@@ -49,9 +104,12 @@ void main()
 let fsShader = `
 precision mediump float;
 uniform vec3 color;
+varying vec2 fragtexture;
+uniform sampler2D fragSampler;
 void main()
 {
-    gl_FragColor = vec4(color, 1.0);
+
+    gl_FragColor = texture2D(fragSampler, fragtexture);
 }
 `;
 
@@ -74,15 +132,21 @@ if (!webgl.getProgramParameter(program, webgl.LINK_STATUS)) {
 let Position = webgl.getAttribLocation(program, "vecposition");
 console.log(Position);
 webgl.enableVertexAttribArray(Position);
+webgl.bindBuffer(webgl.ARRAY_BUFFER, buffer);
 webgl.vertexAttribPointer(Position, 3, webgl.FLOAT, false, 0, 0);
 webgl.clearColor(0.0, 0.0, 1.0, 1.0);
 webgl.clear(webgl.COLOR_BUFFER_BIT);
+
+let textPos = webgl.getAttribLocation(program, 'vtexture');
+webgl.enableVertexAttribArray(textPos);
+webgl.bindBuffer(webgl.ARRAY_BUFFER, texcoBuffer);
+webgl.vertexAttribPointer(textPos, 2, webgl.FLOAT, false, 0, 0);
 let angle = Math.PI / 500;
 let unloc = webgl.getUniformLocation(program, "rotateY");
 let unloc2 = webgl.getUniformLocation(program, "rotateX");
 let unloc3 = webgl.getUniformLocation(program, "rotateZ");
 let colorloc = webgl.getUniformLocation(program, "color");
-webgl.drawArrays(webgl.TRIANGLE_FAN, 0, 4);
+// webgl.drawArrays(webgl.TRIANGLE_FAN, 0, 4);
 function draw() {
   webgl.clear(webgl.COLOR_BUFFER_BIT);
   webgl.uniformMatrix4fv(unloc, false, rotateY(angle));
